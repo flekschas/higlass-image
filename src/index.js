42fdf561e51ede1543874e6f21f393c83899a353
Fritz Lekschas <service@lekschas.de>
2018-04-15 17:26:06 -0400

    Init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import register from 'higlass-register';
+
+import TiledImageTrack from './TiledImageTrack';
+
+register({
+  name: 'TiledImageTrack',
+  track: TiledImageTrack,
+  config: TiledImageTrack.config,
+});
